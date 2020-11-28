4e7a50bbab2347403316f3fbf9b161c97c48e55e
Martin Štrekelj <49723893+MartinStrekelj@users.noreply.github.com>
2020-11-28 15:43:26 +0100

    Add .sql, add registration, add profile model

--- frontend/views/shop/shop.php
+++ frontend/views/shop/shop.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= CSS_URL . "tailwind.css" ?>" />
-    <title>RentOffice</title>
-</head>
-    <body>
-        <p1 class="text-blue-600 text-lg"> Hello world!</p1>
-    </body>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Index page</title>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+  </body>
 </html>
