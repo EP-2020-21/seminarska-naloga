33b2ace470c61dbffa65c49cf6f7a85b3c0a458e
MartinStrekelj <martin.strekelj123@gmail.com>
2020-12-03 19:54:58 +0100

    Change Jquery for AlpineJS

--- frontend/views/shop/index.php
+++ frontend/views/shop/index.php
@@ -1,60 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>STORE</title>
-    <link rel="stylesheet" href="<?= CSS_URL . "tailwind.css" ?>">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="<?= CSS_URL . "_shop.css"?>">
-  </head>
-  <body class="bg_pattern">
-    <a class="p-2 fixed bottom-0 right-0 bg-blue-700 uppercase text-white w-full sm:w-32" href="#nav">Nazaj na vrh</a>
+	<head>
+		<meta charset="UTF-8" />
+		<title>STORE</title>
+		<link rel="stylesheet" href="<?=CSS_URL . "tailwind.css"?>">
+		<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+		<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+		<link rel="stylesheet" href="<?=CSS_URL . "_shop.css"?>">
+	</head>
+	<body class="bg_pattern">
+		<a class="p-2 fixed bottom-0 right-0 bg-blue-700 uppercase text-white w-full sm:w-32" href="#nav"></a>
 
-    <!-- NAVIGATION -->
-    <?php include_once "nav.php"; ?>
-    <!--  -->
-    <?php include_once "category.php"; ?>
-    <main class="lg:container shadow-2xl mx-auto bg-white lg:mt-3 relative lg:bottom-10" style="margin-bottom: 40px;">
-      <!-- FILTERS TODO-->
-      <!-- FEATURE ARTIKLI -->
-      <?php include_once "featureditems.php"; ?>
-      <!-- OSTALI ARTIKLI -->
-      <?php include_once "items.php"; ?>
-    </main>
+		<!-- NAVIGATION -->
+		<?php include_once "nav.php";?>
+		<!--  -->
+		<?php include_once "category.php";?>
+		<main class="lg:container shadow-2xl mx-auto bg-white lg:mt-3 relative lg:bottom-10" style="margin-bottom: 40px;">
+			<!-- FILTERS TODO-->
 
-    <!-- FOOTER -->
-    
-    <!-- LIBS  -->
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script>
-      // MOBILE MENU OVERLAY
-      const overlay = document.getElementById('mobile-menu-overlay');
-      const toggleMenu = (show) => {
-          if (show && overlay.classList.contains('w-0')){
-              overlay.classList.remove('w-8');
-              overlay.classList.add('w-screen');
-          } else {
-              overlay.classList.remove('w-screen');
-              overlay.classList.add('w-0')
-          }
-      };
+			</div>
+			<!-- FEATURE ARTIKLI -->
+			<?php include_once "featureditems.php";?>
+			<!-- OSTALI ARTIKLI -->
+			<?php include_once "items.php";?>
+		</main>
 
-      // FEATURED ITEMS
-      var mySwiper = new Swiper('.swiper-container', {
-        loop: true,
-        speed: 600,
-        autoplay: {
-          delay: 3000,
-        },
-        fadeEffect: {
-          crossFade: true
-        },
-      })
+		<!-- FOOTER -->
 
-    </script>
-  </body>
+		<!-- LIBS  -->
+		<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+		<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+		<!-- Swiper -->
+		<script src="<?= JS_URL . "swiper.js"?>"> </script>
+		<script src="<?= JS_URL . "mobile-overlay.js"?>"> </script>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -10,42 +10,48 @@ require_once($CONTROLLER_PATH.'ProfileController.php');
 
 // Defining constant paths
 define("BASE_URL", $_SERVER["SCRIPT_NAME"] . "/");
-// define("IMAGES_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "frontend/static/images/");
-define("CSS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "frontend/static/");
+define("IMAGES_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "frontend/static/images/");
+define("CSS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "frontend/static/css/");
+define("JS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "frontend/static/js/");
 
 $path = isset($_SERVER["PATH_INFO"]) ? trim($_SERVER["PATH_INFO"], "/") : "";
 
 // Routing to controllers
 $urls = [
-  "" => function(){
-    return ShopController::showIndexPage();
-  },
+	"" => function(){
+		return ShopController::showIndexPage();
+	},
 
-  "register" => function(){
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-      ProfileController::Register();
-    } else {
-      ProfileController::RegisterForm();
-    }
-  },
+	"register" => function(){
+		if ($_SERVER["REQUEST_METHOD"] == "POST"){
+			ProfileController::Register();
+		} else {
+			ProfileController::RegisterForm();
+		}
+	},
 
-  "login" => function(){
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-      ProfileController::Login();
-    } else {
-      ProfileController::LoginForm();
-    }
-  }
+	"login" => function(){
+		if ($_SERVER["REQUEST_METHOD"] == "POST"){
+			ProfileController::Login();
+		} else {
+			ProfileController::LoginForm();
+		}
+	},
+
+	// API
+	"api/getItems" => function(){
+		ShopController::getItems();
+	}
 
 ];
 
 // Route to controller if exists else sent 404
 try {
-  if (isset($urls[$path])) {
-     $urls[$path]();
-  } else {
-      echo "No controller for '$path'";
-  }
+	if (isset($urls[$path])) {
+		$urls[$path]();
+	} else {
+		echo "No controller for '$path'";
+	}
 } catch (Exception $e) {
-  echo "An error occurred: <pre>$e</pre>";
+	echo "An error occurred: <pre>$e</pre>";
 };
